1430ef1ffe1584754194a9e8b671e349d1465d3a
Johee Chung <johchung@microsoft.com>
2021-04-10 22:57:23 -0500

    First iteration of PP tracker with space invader ascii art

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const fetch = require('isomorphic-fetch');
+const Discord = require('discord.js');
+
+let channelId;
+let mee6DashboardId;
+
+if (process.env.NODE_ENV !== 'production') {
+  channelId = process.env.DEV_DISCORD_CHANNEL_ID;
+  mee6DashboardId = process.env.DEV_MEE6_DASHBOARD_ID;
+} else {
+  channelId = process.env.PROD_DISCORD_CHANNEL_ID;
+  mee6DashboardId = process.env.PROD_MEE6_DASHBOARD_ID;
+}
+
+const numberOfLeaders = 10;
+const leaderboardUrl = `https://mee6.xyz/api/plugins/levels/leaderboard/${mee6DashboardId}?limit=${numberOfLeaders}&page=0`;
+const client = new Discord.Client();
+
+client.login(process.env.DISCORD_BOT_TOKEN).then(async () => {
+  console.log("Logged in");
+
+  const channel = await client.channels.fetch(channelId);
+  if(channel){
+    fetch(leaderboardUrl)
+      .then(res => res.json())
+      .then(({ players }) => 
+        `\nPP LEADERBOARD\n${players.sort((a, b) => b.level - a.level).map(p => (
+          `<@${p.id}>: ${'👾'.repeat(p.level)}`
+        )).join('\n')}
+                                  !
+                                  !
+                                  !
+                           |--__| ^ |__--|`
+      )
+      .then(leaderboardMsg => {
+        channel.send(leaderboardMsg)
+          .catch(err => console.error({ err }))
+          .then(() => client.destroy());
+      })
+      .catch(err => console.error({ err }));  
+  } else {
+      console.error("Unable to get channel");
+      //if the bot doesn't have guild with the id guildid
+      // or if the guild doesn't have the channel with id channelid
+  }
+})
+.catch(err => console.error({ err }));
