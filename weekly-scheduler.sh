baadde546d8b4058394c36a1b497658417047be6
Johee Chung <johchung@microsoft.com>
2021-04-11 09:39:39 -0500

    Update bash script to handle arguments

--- weekly-scheduler.sh
+++ weekly-scheduler.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo "Running weekly ($(date +%u)) scheduler script on ${env}"
+echo "Running weekly ($(date +%u)) scheduler script on $1"
 if [ "$(date +%u)" = 7 ]
 then
-    if [ "$(env)" = "prod" ]
+    if [ $1 = "prod" ]
     then
+        echo "[LOG] Bash script running on prod"
         NODE_ENV=prod USERS=10 node index.js
     else
+        echo "[LOG] Bash script not running on prod"
         NODE_ENV=dev USERS=10 node index.js
     fi
+else
+    echo "[LOG] Run did not match schedule"
 fi
